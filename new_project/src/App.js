94eee698f81204395264098b471fb79cd15d33e0
pandit <dhamdherepandit@gmail.com>
2023-01-30 10:42:19 +0530

    delete button added

--- new_project/src/App.js
+++ new_project/src/App.js
@@ -5,7 +5,11 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import AddIcon from '@mui/icons-material/Add';
 import { useState } from 'react';
-import Fields from './components/Fields';
+// import Fields from './components/Fields';
+
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 
 
@@ -20,6 +24,12 @@ function App() {
     setEmail("");
   }
 
+
+  const removeItem = (index) => {
+    let arr = data;
+    arr.splice(index, 1)
+    setData([...arr]);
+  }
   return (
     <div className="App">
       <Header />
@@ -41,19 +51,30 @@ function App() {
           </Button>
         </Stack>
       </div>
+
       <div className='data'>
         <div className='data_val'>
           <h4>Name</h4>
           <h4>Email</h4>
           <h4>Remove</h4>
         </div>
+
         {
           data.map((element, index) => {
             return (
-              <Fields name = {element.name} email = {element.email} index={index}/ >
+              <div key={index} className='data_val'>
+                <h4>{element.name}</h4>
+                <h4>{element.email}</h4>
+                <Stack>
+                  <Button onClick={() => removeItem(index)} variant="contained" color="error">
+                    <DeleteIcon />
+                  </Button>
+                </Stack>
+              </div>
             )
           })
         }
+
       </div>
     </div>
   );

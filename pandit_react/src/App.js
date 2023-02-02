6d144107ab4a86b7056c32ecdd0de98214cbd75c
pandit <dhamdherepandit@gmail.com>
2023-02-03 01:30:54 +0530

    added component & links react router dom v6

--- pandit_react/src/App.js
+++ pandit_react/src/App.js
@@ -3,6 +3,9 @@ import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Error from './components/Error';
+import Mail from './components/Mail';
+import Insta from './components/Insta';
 import {
   BrowserRouter as Router,
   Routes,
@@ -16,7 +19,7 @@ function App() {
 
   return (
     <Router>
-      <>
+      <div>
         <Header />
         <Routes>
 
@@ -26,11 +29,16 @@ function App() {
           <Route path="/about" element={<About />} />
 
           {/* Contact */}
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} >
+            <Route path="insta" element={<Insta />} />
+            <Route path="mail" element={<Mail />} />
 
+          </Route>
 
+          {/* error */}
+          <Route path="*" element={<Error />} />
         </Routes>
-      </>
+      </div>
     </Router>
   );
 }
--- pandit_react/src/components/Header.js
+++ pandit_react/src/components/Header.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 const Header = () => {
+
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      textDecoration: isActive ? "none" : "underline",
+      color: isActive ? "red" : "white",
+      fontSize: "25px",
+      display: "flex",
+      justifyContent: "space-between"
+    };
+  };
+
   return (
     <div>
       <div className="header">
-       <Link to = {'/'}> <h1 style={{color: "whie"}}>Home</h1></Link>
-       <Link  to = {'/about'}> <h1 style={{color: "whie"}}>About</h1></Link>
-       <Link  to = {'/contact'}> <h1 style={{color: "whie"}}>Contact</h1></Link>
+        <NavLink style={navLinkStyles} to={'/'}>Home</NavLink>
+
+        <NavLink style={navLinkStyles} to={'/about'}>About</NavLink>
+
+        <NavLink style={navLinkStyles} to={'/contact'}>Contact</NavLink>
+
+
+
+        {/* <Link to={'/about'}> <h1 style={{ color: "white" }}>About</h1></Link>
+        <Link to={'/contact'}> <h1 style={{ color: "white" }}>Contact</h1></Link> */}
+
+
+        {/* <Link  to = {'/error'}> <h1 style={{color: "white"}}>Error</h1></Link> */}
       </div>
     </div>
   )

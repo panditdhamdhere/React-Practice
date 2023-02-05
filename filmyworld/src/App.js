da9cf4ca5f0cbe150fe94af41f88815f9acb2513
pandit <dhamdherepandit@gmail.com>
2023-02-05 23:00:51 +0530

    new form added

--- filmyworld/src/App.js
+++ filmyworld/src/App.js
@@ -1,11 +1,17 @@
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Addmovie from "./components/Addmovie";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Cards />
+      <Routes>
+       <Route path="/" element={<Cards/>}/>
+       <Route path="/addmovie" element={<Addmovie />} />
+      </Routes>
+
     </div>
   );
 }

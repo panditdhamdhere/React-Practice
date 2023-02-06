0cc03e3c3abd0d4d539773a7beab25276fe5d68d
pandit <dhamdherepandit@gmail.com>
2023-02-06 22:18:03 +0530

    data added

--- filmyworld/src/components/Cards.js
+++ filmyworld/src/components/Cards.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import ReactStars from 'react-stars'
-import { TailSpin } from 'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
+import { getDocs } from 'firebase/firestore';
+// import { getDocs } from 'firebase/firestore';
+import { moviesRef } from './firebase/Firebase';
 // import reactStars from "react-stars"
 const Cards = () => {
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function getData() {
+            setLoading(true);
+            const _data = await getDocs(moviesRef);
+            _data.forEach((doc) => {
+                setData((prv) => [...prv, doc.data()])
+            })
 
+            setLoading(false);
         }
         getData()
     }, [])
@@ -16,30 +25,30 @@ const Cards = () => {
 
     return (
         <div className='flex flex-wrap justify-between p-3 mt-2'>
-            {loading ? <div className='w-full flex justify-center items-center h-96'><TailSpin height={40} color="white" /> </div> :
-    
-                    data.map((e, i) => {
-                        return (
+            {loading ? <div className='w-full flex justify-center items-center h-96'><ThreeDots height={40} color="white" /> </div> :
 
-                            <div key={i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-75'>
-                                <img className='h-72' src={e.image} alt='movie poster' />
-                                <h1><span className='text-blue-500'>Name: </span>{e.title}</h1>
-                                <h1 className='flex items-center mr-1'>
-                                    <span className='text-blue-500'>Rating:</span>{e.rating}
-                                    <ReactStars size={20}
-                                        half={true}
-                                        value={5}
-                                        edit={false}
-                                    />
-                                </h1>
+                data.map((e, i) => {
+                    return (
 
-                                <h1><span className='text-blue-500'>Year: </span>{e.year}</h1>
-                            </div>
-                        );
-                    })
-                }
-            
-            
+                        <div key={i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-75'>
+                            <img className='h-72' src={e.image} alt='movie poster' />
+                            <h1><span className='text-blue-500'>Name: </span>{e.title}</h1>
+                            <h1 className='flex items-center mr-1'>
+                                <span className='text-blue-500'>Rating:</span>{e.rating}
+                                <ReactStars size={20}
+                                    half={true}
+                                    value={5}
+                                    edit={false}
+                                />
+                            </h1>
+
+                            <h1><span className='text-blue-500'>Year: </span>{e.year}</h1>
+                        </div>
+                    );
+                })
+            }
+
+
         </div>
     )
 }

5116fe8037b507f7e3e5a165b28e4532e266398a
pandit <dhamdherepandit@gmail.com>
2023-02-06 21:15:34 +0530

    spinner added

--- filmyworld/src/components/Cards.js
+++ filmyworld/src/components/Cards.js
@@ -1,31 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactStars from 'react-stars'
+import { TailSpin } from 'react-loader-spinner';
 // import reactStars from "react-stars"
 const Cards = () => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function getData() {
+
+        }
+        getData()
+    }, [])
 
 
     return (
         <div className='flex flex-wrap justify-between p-3 mt-2'>
-            {data.map((e, i) => {
-return (
-         
-                <div key={i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-75'>
-                    <img className='h-72' src={e.img} alt='movie poster' />
-                    <h1><span className='text-blue-500'>Name: </span>{e.name}</h1>
-                    <h1 className='flex items-center mr-1'>
-                        <span className='text-blue-500'>Rating:</span>{e.rating}
-                    <ReactStars size={20}
-                    half={true}
-                    value={e.rating}
-                    edit={false}
-                    />
-                    </h1>
+            {loading ? <div className='w-full flex justify-center items-center h-96'><TailSpin height={40} color="white" /> </div> :
+    
+                    data.map((e, i) => {
+                        return (
 
-                    <h1><span className='text-blue-500'>Year: </span>{e.year}</h1>
-                </div>
-            )})}
+                            <div key={i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-75'>
+                                <img className='h-72' src={e.image} alt='movie poster' />
+                                <h1><span className='text-blue-500'>Name: </span>{e.title}</h1>
+                                <h1 className='flex items-center mr-1'>
+                                    <span className='text-blue-500'>Rating:</span>{e.rating}
+                                    <ReactStars size={20}
+                                        half={true}
+                                        value={5}
+                                        edit={false}
+                                    />
+                                </h1>
 
+                                <h1><span className='text-blue-500'>Year: </span>{e.year}</h1>
+                            </div>
+                        );
+                    })
+                }
+            
+            
         </div>
     )
 }

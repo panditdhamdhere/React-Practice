9e4c7555226bfa3943bc3b6b7f742757c07baa61
pandit <dhamdherepandit@gmail.com>
2023-02-07 17:02:03 +0530

    made responsive

--- filmyworld/src/components/Cards.js
+++ filmyworld/src/components/Cards.js
@@ -24,14 +24,14 @@ const Cards = () => {
 
 
     return (
-        <div className='flex flex-wrap justify-between p-3 mt-2'>
+        <div className='flex flex-wrap justify-between px-3 mt-2'>
             {loading ? <div className='w-full flex justify-center items-center h-96'><ThreeDots height={40} color="white" /> </div> :
 
                 data.map((e, i) => {
                     return (
 
                         <div key={i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-75'>
-                            <img className='h-72' src={e.image} alt='movie poster' />
+                            <img className='h-60 md:h-72' src={e.image} alt='movie poster' />
                             <h1><span className='text-blue-500'>Name: </span>{e.title}</h1>
                             <h1 className='flex items-center mr-1'>
                                 <span className='text-blue-500'>Rating:</span>{e.rating}

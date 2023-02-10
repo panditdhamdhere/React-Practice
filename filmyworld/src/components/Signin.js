54dccc705f2cd285de3518c8eedbe43213973310
pandit <dhamdherepandit@gmail.com>
2023-02-10 21:51:48 +0530

    fixing some errors

--- filmyworld/src/components/Signin.js
+++ filmyworld/src/components/Signin.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react'
-import { TailSpin } from 'react-loader-spinner';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { TailSpin } from "react-loader-spinner";
+import { Link } from "react-router-dom";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth'
+import app from '../firebase/firebase'
+import swal from "sweetalert";
+import { addDoc } from "firebase/firestore";
+import { usersRef } from "../firebase/firebase";
+import { useNavigate } from "react-router-dom";
+
+//import bcrypt from 'bcryptjs';
+// const auth = getAuth(app);
+
+const auth = getAuth(app);
 
 const Signin = () => {
+    const navigate = useNavigate()
     const [form, setForm] = useState({
         name: "",
         mobile: "",
@@ -11,22 +23,87 @@ const Signin = () => {
     })
 
     const [loading, setLoading] = useState(false);
-    const [otpSent, setOtpSent] = useState(true);
+    const [otpSent, setOtpSent] = useState(false);
+    const [OTP, setOTP] = useState("");              //pandit Dhamdhere
+
+    const generateRecaptha = () => {
+        window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+            'size': 'invisible',
+            'callback': (response) => {
+
+            }
+        }, auth)
+    }
+
+
+    const requestOtp = () => {
+        setLoading(true);
+        generateRecaptha();
+        let appVerifier = window.recaptchaVerifier;
+        signInWithPhoneNumber(auth, `+91${form.mobile}`, appVerifier)
+            .then(confirmationResult => {
+                window.confirmationResult = confirmationResult;
+                swal({
+                    text: "OTP Sent",
+                    icon: "success",
+                    buttons: false,
+                    timer: 3000,
+                });
+                setOtpSent(true);
+                setLoading(false);
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+
+    const verifyOTP = () => {
+        try {
+            setLoading(true);
+            window.confirmationResult.confirm(OTP).then((result) => {
+                uploadData();
+                swal({
+                    text: "Sucessfully Registered",
+                    icon: "success",
+                    buttons: false,
+                    timer: 3000,
+                });
+                navigate('/login')
+                setLoading(false);
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const uploadData = async () => {
+        try {
+            const salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(form.password, salt);
+            await addDoc(usersRef, {
+                name: form.name,
+                password: hash,
+                mobile: form.mobile
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
 
     return (
         <div className='w-full flex flex-col mt-8 items-center'>
-                <h1 className='text-xl font-bold mt'>Sign Up</h1>
+            <h1 className='text-xl font-bold mt'>Sign Up</h1>
             {otpSent ?
                 <>
                     <div class="p-2 w-1/3">
                         <div class="relative">
                             <label for="message" class="leading-7 text-sm text-white font-bold">OTP</label>
                             <input
-
                                 id="message"
                                 name="message"
-                                value={form.name}
-                                onChange={(e) => setForm({ ...form, name: e.target.value })}
+                                value={OTP}
+                                onChange={(e) => setOTP(e.target.value)}
                                 class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                             />
                         </div>
@@ -35,13 +112,14 @@ const Signin = () => {
 // button  */}
                     <div className='p-2 w-full md:w-full'>
                         <button
+                            onClick={verifyOTP}
                             class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">{loading ? <TailSpin height={25} color="white" /> : 'Confirm OTP'}</button>
                     </div>
 
                 </>
                 :
                 <>
-                
+
 
                     <div class="p-2 w-1/3">
                         <div class="relative">
@@ -89,7 +167,9 @@ const Signin = () => {
                     </div>
                     <div className='p-2 w-full md:w-full'>
                         <button
-                            class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">{loading ? <TailSpin height={25} color="white" /> : 'Sign Up'}</button>
+                            onClick={request}
+                            class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">
+                            {loading ? <TailSpin height={25} color="white" /> : 'Request OTP'}</button>
                     </div>
                 </>
             }
@@ -97,10 +177,9 @@ const Signin = () => {
                 <p>Already have an account? <Link to={'/lognin'}> <span className='text-blue-500'>Login</span></Link></p>
             </div>
 
-            {/* <div className='mt-40'>
-                <p>Build by Pandit with Love</p>
+            <div id="recaptcha-container">
+
             </div>
-             */}
         </div>
     )
 }

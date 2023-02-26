7dbe7a58a17598b434017c09e993087f6a5e65a3
panditdhamdhere <dhamdherepandit@gmail.com>
2023-02-26 22:57:22 +0530

    learnt ternary operator

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -4,13 +4,25 @@ import Movie from './components/Movie';
 import movies from './movies.json'
 
 function App() {
+
+  let login = false;
+
+
+
   return (
     <div className="App">
+
+      {
+        login == false ? <h1 style={{color:'black'}}>Pandit dada ja rey</h1> :
+        <h2>Aaahhh theek hai</h2>
+
+      }
       <Header />
       <div className="main">
         {
-          movies.map((element) => {
+          movies.map((element, index) => {
             return (<Movie
+              key={index}
               title={element.Title}
               year={element.Year}
               img={element.Poster}

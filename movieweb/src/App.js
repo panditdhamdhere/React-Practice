3226ff2e55cb55c1eee86c3462c91b69428e84be
panditdhamdhere <dhamdherepandit@gmail.com>
2023-02-26 22:23:39 +0530

    new practice

---
+++ movieweb/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import movies from './movies.json'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        {
+          movies.map((element) => {
+            return (<Movie
+              title={element.Title}
+              year={element.Year}
+              img={element.Poster}
+            />)
+
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;

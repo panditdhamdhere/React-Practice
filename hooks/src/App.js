028eb1ba2aee2f010b80ad3dd63f735136844100
panditdhamdhere <dhamdherepandit@gmail.com>
2023-02-28 16:11:08 +0530

    some more practice

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='form'>
+      <Stack spacing={2} direction="row">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button color='success' variant="contained">Add</Button>
+        </Stack>
+      </div>
+
     </div>
   );
 }
---
+++ hooks/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Record keeping</h1>
+    </div>
+  )
+}
+
+export default Header

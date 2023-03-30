3b3210c7c4cca0d207a0a8356bf8aa55af891dd7
panditdhamdhere <dhamdherepandit@gmail.com>
2023-03-30 22:11:37 +0530

    footer added

---
+++ filmyworld/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <p className='font-bold flex '>Made it love by pandit developer</p>
+    </div>
+  )
+}
+
+export default Footer

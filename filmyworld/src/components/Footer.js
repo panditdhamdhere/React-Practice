2237518a7b9820cc9541de23665ce200e5dd5698
panditdhamdhere <dhamdherepandit@gmail.com>
2023-03-31 23:30:18 +0530

    p added

--- filmyworld/src/components/Footer.js
+++ filmyworld/src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <div>
       <p className='font-bold flex '>Made it love by pandit developer</p>
+    <p>All Right Reserve 2023 Pandit Dhamdhere</p>
     </div>
   )
 }
